aea7957df8c613408b5e524bf6dd7c0e592d9b8c
KPizzaman <46193498+KPizzaman@users.noreply.github.com>
2020-06-02 18:12:22 +1000

    Update drawboard.py

--- drawboard.py
+++ drawboard.py
@@ -4,11 +4,14 @@ def gameinit():
     pygame.init()
     screen = pygame.display.set_mode((800,800))
     screen.fill((255,255,255))
-    for i in range(0,800, 100):
-        pygame.draw.rect(screen, (0,0,0), (i,i,100,100), 1)
+    for i in range(0,800, 200):
+        for j in range(0,800,100):
+            pygame.draw.rect(screen, (0,0,0), (i+100*((j/100+1)%2),j,100,100), 0)
     pygame.display.update()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.quit()
                 exit()
+if __name__ == "__main__":
+    gameinit()
