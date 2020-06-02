3866469651cd70cfcd19180ce9247339a5d9e6a5
KPizzaman <46193498+KPizzaman@users.noreply.github.com>
2020-06-02 18:11:50 +1000

    Update objectorientedchess.py

--- objectorientedchess.py
+++ objectorientedchess.py
@@ -1,41 +1,54 @@
-import pygame
-import sys
-def gameinit():
-    pygame.init()
-    screen = pygame.display.set_mode((800,800))
-    screen.fill((255,255,255))
-    for i in range(0,800, 100):
-        pygame.draw.rect(screen, (0,0,0), (i,i,100,100), 1)
-    pygame.display.update()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.quit()
-                exit()
+import drawboard
 
 
+# The piece class which holds the positionsm colour and state of every piece
 class piece:
     def __init__(self, xcord, ycord, colour):
         self.xcord = xcord
         self.ycord = ycord
         self.taken = False
-        self.colour = "white"
-class pawn(piece):
-    def __init__(self, xcord, ycord, colour):    
-        super().__init__(xcord, ycord, colour)
+        self.colour = True # true = white ; false = black Not a racist remark
+
+# The king class
 class king(piece):
     def __init__(self, xcord, ycord, colour):    
         super().__init__(xcord, ycord, colour)
+    def move(self, newx, newy): # move function
+        if abs(newx-self.xcord)<= 1 and abs(newy-self.ycord)<= 1 : # Makes sure king can't move more than one block out, king can stay in spot tho
+            self.xcord = newx       
+            self.ycord = newy
+
 class queen(piece):
     def __init__(self, xcord, ycord, colour):    
         super().__init__(xcord, ycord, colour)
-class bishop(piece):
+    def move(self, newx, newy): 
+        self.xcord = newx       
+        self.ycord = newy
+
+class bishop(piece):    
     def __init__(self, xcord, ycord, colour):    
         super().__init__(xcord, ycord, colour)
+    def move(self, newx, newy): 
+        self.xcord = newx       
+        self.ycord = newy
+
 class knight(piece):
     def __init__(self, xcord, ycord, colour):    
         super().__init__(xcord, ycord, colour)
+    def move(self, newx, newy): 
+        self.xcord = newx       
+        self.ycord = newy
+
 class rook(piece):
     def __init__(self, xcord, ycord, colour):    
         super().__init__(xcord, ycord, colour)
+    def move(self, newx, newy): 
+        self.xcord = newx       
+        self.ycord = newy
 
+class pawn(piece):
+    def __init__(self, xcord, ycord, colour):    
+        super().__init__(xcord, ycord, colour)
+    def move(self, newx, newy): 
+        self.xcord = newx       
+        self.ycord = newy
