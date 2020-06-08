3580e3353e9ff856a4230a9486fd3bb39818afa1
KPizzaman <46193498+KPizzaman@users.noreply.github.com>
2020-06-09 06:23:19 +1000

    Update drawboard.py

--- drawboard.py
+++ drawboard.py
@@ -16,7 +16,6 @@ def playerdrag(player, event, screen):
 
 class piece(object):
     def __init__(self, xcord, ycord, colour):
-        self.surface = pygame.image.load(os.path.join("Desktop\SDD\Chess", "pawn.png"))
         self.surface.convert_alpha()
         self.surface = pygame.transform.scale(self.surface, (100,100))
         width, height = self.surface.get_size()
@@ -28,34 +27,38 @@ class piece(object):
         print(self.rect)
         self.dragging = False
 
-    def handle_click(self, event):
-        mousex, mousey = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(mousex, mousey ):
-                print("cock")
-                if event.button == 1:     
-                    self.dragging = True
-        if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    self.dragging = False
-                    self.xcord = round(self.rect.left/100+1)
-                    self.ycord = round(self.rect.top/(-100)+8)
-                    print(self.xcord, self.ycord)
-                    self.rect.left = (self.xcord-1)*100
-                    self.rect.top = (8-self.ycord)*100
-
     def mousedrag(self, dragging):
         if dragging:
             self.rect.left , self.rect.top = pygame.mouse.get_pos()
             self.rect.left -= self.rect.width/2
             self.rect.top -= self.rect.height/2
 
+    def handle_click(self, event):
+        mousex, mousey = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(mousex, mousey ):
+                if event.button == 1:     
+                    self.dragging = True
 
+        if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.dragging = False
+                    newx = round(self.rect.left/100+1)
+                    newy = round(self.rect.top/(-100)+8)
+                    boo = self.move(newx,newy)
+                    if boo:
+                        self.xcord = round(self.rect.left/100+1)
+                        self.ycord = round(self.rect.top/(-100)+8)
+                        self.rect.left = (self.xcord-1)*100
+                        self.rect.top = (8-self.ycord)*100
+                    else: 
+                        self.rect.left = (self.xcord-1)*100
+                        self.rect.top = (8-self.ycord)*100
 
     def draw(self, surface):
         surface.blit(self.surface, (self.rect.left,self.rect.top))
 
-
+    
 def gameinit():
     # Initialise pygame
     pygame.init()
@@ -69,20 +72,18 @@ def gameinit():
     clock = pygame.time.Clock()
     pygame.draw
     # Loop that checks for any event in the game
-    player = piece(1,2, True)
-    playeer = piece(7,7, True)
+    return screen, clock
+def gamerun(players, screen, clock):
     while True:
         for event in pygame.event.get():
             # Checks if window tries to be close
             if event.type == pygame.QUIT:
             # Closes the program altogether
                 return
-            if event.type == pygame.MOUSEMOTION:
-                print(pygame.mouse.get_pos())
             
             chessboard(screen)
-            playerdrag(player, event, screen)
-            playerdrag(playeer, event, screen)
+            for i in players:
+                playerdrag(i, event, screen)
             pygame.display.update()
             clock.tick(60)
 
