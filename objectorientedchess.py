7c2fe95a949565ca05caa22f71d899e2eb803707
KPizzaman <46193498+KPizzaman@users.noreply.github.com>
2020-06-16 19:39:56 +1000

    Update objectorientedchess.py

--- objectorientedchess.py
+++ objectorientedchess.py
@@ -1,13 +1,120 @@
 import pygame
 import os
 import math
-from drawboard import *
 
 
-# The piece class which holds the positionsm colour and state of every piece
+# The piece class which holds the positions colour and state of every piece
+turn = True
+check = False
+def ccheck(players, king):
+    global check
+    for i in players:
+        if capture(i, players, king.xcord, king.ycord):
+            check = True
+    print(check)
+def chessboard(screen):
+    screen.fill((255,255,255))
+    for i in range(0,800, 200): 
+        for j in range(0,800,100):
+            pygame.draw.rect(screen, (209,139,71), (i+100*((j/100+1)%2),j,100,100), 0)
 
+def playerdrag(player, event, screen, players):
+    player.handle_click(event, players)
+    player.mousedrag(player.dragging)
+    player.draw(screen)
 
+class piece(object):
+    def __init__(self, xcord, ycord, colour):
+        self.xcord = xcord
+        self.ycord = ycord
+        self.taken = False
+        self.colour = colour # true = white ; false = black Not a racist remark
+        self.dragging = False
 
+    def mousedrag(self, dragging):
+        if dragging:
+            self.rect.left , self.rect.top = pygame.mouse.get_pos()
+            self.rect.left -= self.rect.width/2
+            self.rect.top -= self.rect.height/2
+
+    def handle_click(self, event, players):
+        mousex, mousey = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(mousex, mousey ):
+                if event.button == 1:     
+                    self.dragging = True
+
+        if event.type == pygame.MOUSEBUTTONUP:
+                if self.rect.collidepoint(mousex, mousey ):
+                    if event.button == 1:
+                        self.dragging = False
+                        newx = round(self.rect.left/100+1)
+                        newy = round(self.rect.top/(-100)+8)
+                        boo = self.move(newx,newy)
+                        if boo and check is False:
+                            self.xcord = round(self.rect.left/100+1)
+                            self.ycord = round(self.rect.top/(-100)+8)
+                            self.rect.left = (self.xcord-1)*100
+                            self.rect.top = (8-self.ycord)*100
+                        else: 
+                            self.rect.left = (self.xcord-1)*100
+                            self.rect.top = (8-self.ycord)*100
+                        ccheck(players, players[8])
+                        
+
+    def draw(self, surface):
+        if not self.taken:
+            surface.blit(self.surface, (self.rect.left,self.rect.top))
+
+    
+def gameinit():
+    # Initialise pygame
+    pygame.init()
+    # creates the main screen with a dimension of 800x800
+    screen = pygame.display.set_mode((800,800))
+    # draws the chessboard pattern onto background
+    chessboard(screen)
+    # updates screen so the drawing appears on it
+    pygame.display.update()
+    # sets variable clock to the the tick speed of the game, allows for abstraction and cleaner code
+    clock = pygame.time.Clock()
+    pygame.draw
+    # Loop that checks for any event in the game
+    return screen, clock
+def gamerun(players, screen, clock, check):
+    while True:
+        for event in pygame.event.get():
+            # Checks if window tries to be close
+            if event.type == pygame.QUIT:
+            # Closes the program altogether
+                return
+            
+            chessboard(screen)
+            for i in players:
+                playerdrag(i, event, screen, players)
+            pygame.display.update()
+            clock.tick(75)
+
+def capture(self, players, newx, newy):
+    global turn
+    if self.colour is turn:
+        for i in players:
+            x = i.xcord
+            y = i.ycord
+            if x == newx and y == newy:
+                if i.colour != self.colour and type(i) is not king:
+                    i.taken = True
+                    i.xcord, i.ycord = 0,0
+                    turn = not turn
+                    print(turn)
+                    return True
+                elif type(i) is king and i.colour != self.colour: 
+                    return True
+                else:
+                    return  
+        turn = not turn
+        print(turn)
+        return True
 # The king class
 class king(piece):
     def __init__(self, xcord, ycord, colour):
@@ -24,8 +131,9 @@ class king(piece):
     def move(self, newx, newy): # move function
         xdif = abs(newx-self.xcord)
         ydif = abs(newy-self.ycord)
-        if xdif <= 1 and ydif <= 1 and xdif + ydif != 0: # Makes sure king can't move more than one block out
-            return True
+        if xdif <= 1 and ydif <= 1 and xdif + ydif != 0: # Makes sure king can't move more than one block out    
+           if capture(self, players, newx, newy):
+               return True
 
 def diagonal(self, newx, newy, players, stepx, stepy):
     if abs(newx - self.xcord) == abs(newy-self.ycord):
@@ -80,9 +188,11 @@ class queen(piece):
     def move(self, newx, newy): 
         stepx, stepy = getstep(self, newx, newy)
         if straight(self, newx, newy, players, stepx, stepy):
-            return True
+            if capture(self, players, newx, newy):
+               return True
         if diagonal(self, newx, newy, players, stepx, stepy):
-            return True 
+            if capture(self, players, newx, newy):
+               return True
         
 class bishop(piece):    
     def __init__(self, xcord, ycord, colour):
@@ -98,7 +208,8 @@ class bishop(piece):
     def move(self, newx, newy): 
         stepx, stepy = getstep(self, newx, newy)
         if diagonal(self, newx, newy, players, stepx, stepy):
-            return True
+            if capture(self, players, newx, newy):
+               return True
 
 class knight(piece):
     def __init__(self, xcord, ycord, colour):  
@@ -115,7 +226,8 @@ class knight(piece):
         xdif = abs(newx-self.xcord)
         ydif = abs(newy-self.ycord)
         if xdif != 0 and ydif != 0 and xdif + ydif == 3:
-            return True
+            if capture(self, players, newx, newy):
+               return True
 
 class rook(piece):
     def __init__(self, xcord, ycord, colour):
@@ -131,7 +243,8 @@ class rook(piece):
     def move(self, newx, newy): 
         stepx, stepy = getstep(self, newx, newy)
         if straight(self, newx, newy, players, stepx, stepy):
-            return True
+            if capture(self, players, newx, newy):
+               return True
 
 class pawn(piece):
     def __init__(self, xcord, ycord, colour): 
@@ -144,30 +257,66 @@ class pawn(piece):
         self.surface = pygame.transform.scale(self.surface, (100,100))
         width, height = self.surface.get_size()
         self.rect = pygame.rect.Rect(((xcord-1)*100, (8-ycord)*100, width, height))
+    def checkpiece(self, players, newx, newy):
+        global turn
+        for i in players:
+            if self.colour is turn:
+                if i.xcord == newx and i.ycord == newy:
+                    return 
+                turn = not turn
+                return True
     def move(self, newx, newy): 
+        global turn
         if self.colour:
             if newx == self.xcord:
                 if self.ycord == 2 and  0 < newy - self.ycord <= 2:
-                    return True
+                    if self.checkpiece(players, newx, newy):
+                        return True
                 elif newy - self.ycord == 1:
-                    return True
-        elif newx == self.xcord:
-            if self.ycord == 7 and  0 < self.ycord - newy <= 2:
-                return True
-            elif self.ycord - newy == 1:
-                return True
+                    if self.checkpiece(players, newx, newy):
+                        return True
+            if abs(newx-self.xcord) == 1 and newy - self.ycord == 1:
+                for i in players:
+                    if i.xcord == newx and i.ycord == newy and self.colour is turn:
+                        i.taken = True
+                        i.xcord, i.ycord = 0,0
+                        turn = not turn
+                        return True
+                
+        else:
+            if abs(newx-self.xcord) == 1 and  self.ycord - newy == 1:
+                for i in players:
+                    if i.xcord == newx and i.ycord == newy and self.colour is turn:
+                        turn = not turn
+                        i.taken = True
+                        i.xcord, i.ycord = 0,0
+                        return True
+
+
+            if newx == self.xcord:
+                if self.ycord == 7 and  0 < self.ycord - newy <= 2:
+                    if self.checkpiece(players, newx, newy):
+                        return True    
+                elif self.ycord - newy == 1:
+                    if self.checkpiece(players, newx, newy):
+                        return True
+
+
+
+
+
 
 
 gameinit()
 screen, clock = gameinit()
 players = [pawn(i, 2, True) for i in range(1,9)]
+players.append(king(5,1, True))
 for i in range(1,9):
     players.append(pawn(i,7,False))
 players.append(rook(1,1, True))
 players.append(knight(2,1, True))
 players.append(bishop(3,1, True))
 players.append(queen(4,1, True))   
-players.append(king(5,1, True))
 players.append(rook(8,1, True))
 players.append(knight(7,1, True))
 players.append(bishop(6,1, True))
@@ -180,5 +329,4 @@ players.append(king(5,8, False))
 players.append(rook(8,8, False))
 players.append(knight(7,8, False))
 players.append(bishop(6,8, False))
-
-gamerun(players, screen, clock)
+gamerun(players, screen, clock, check)
